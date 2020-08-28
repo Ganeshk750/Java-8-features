cfe6374619be2c12e77c32e337a9c80dd8ef3709
Ganeshk750 <ganesh9ot@gmail.com>
2020-08-28 12:43:55 +0530

    # Adding filter usecase Consumer Functional Interface

--- src/com/functionalInterface/ConsumerExample2.java
+++ src/com/functionalInterface/ConsumerExample2.java
@@ -24,5 +24,22 @@ public class ConsumerExample2 {
         //Loop through all the instructor and print their name and their courses
         Consumer<Instructor> c3 = (s1) -> System.out.println(s1.getCourses());
         instructors.forEach(c2.andThen(c3));
+
+        //Loop through all the instructor and print out their name if the years of experience is > 10
+        System.out.println("-----------------------");
+        instructors.forEach(s1 -> {
+            if(s1.yearOfExperience > 10){
+                c1.accept(s1);
+            }
+        });
+
+        // Loop through all the instructor and print out their name and yearOfExperience if year of experience is > 5 and teaches course online
+        System.out.println("======================");
+        instructors.forEach(s1 ->{
+            if(s1.yearOfExperience > 5 && !s1.isOnlineCourses()){
+                c1.andThen(c2).accept(s1);
+            }
+        });
+
     }
 }
