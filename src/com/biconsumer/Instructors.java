6c819e650e9161876252a14f12b889955a356e5a
Ganeshk750 <ganesh9ot@gmail.com>
2020-08-29 13:19:49 +0530

    # Adding instructor and Instructors class

---
+++ src/com/biconsumer/Instructors.java
@@ -0,0 +1,26 @@
+package com.biconsumer;
+
+import com.functionalInterface.Instructor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Instructors {
+    public static List<com.functionalInterface.Instructor> getAll() {
+
+        com.functionalInterface.Instructor instructor1 = new com.functionalInterface.Instructor("Mike", 10, "Software Developer", "Male",
+                true, Arrays.asList("Java Programming", "C++ Programming", "Python Programming"));
+        com.functionalInterface.Instructor instructor2 = new com.functionalInterface.Instructor("Jenny", 5, "Engineer", "Female",
+                false, Arrays.asList("Multi.Threaded Programming", "CI/CD", "Unit Testing"));
+        com.functionalInterface.Instructor instructo3 = new com.functionalInterface.Instructor("Gane", 6, "Manager", "Male",
+                false, Arrays.asList("C Programming", "C++ Programming", "React Native"));
+        com.functionalInterface.Instructor instructor4 = new com.functionalInterface.Instructor("Anthony", 15, "Senior Developer", "Male",
+                true, Arrays.asList("Java Programming", "Angular Programming", "React Native"));
+        com.functionalInterface.Instructor instructor5 = new com.functionalInterface.Instructor("syed", 15, "Software Developer", "Male",
+                true, Arrays.asList("Java Programming", "Java Multi.Threaded Programming", "React Native"));
+
+        List<Instructor> list = Arrays.asList(instructor1, instructor2, instructo3, instructor4, instructor5);
+        return list;
+       }
+
+}
