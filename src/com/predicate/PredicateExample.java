74e9f786882318ab7c0b400ca9810a0afbd3bb77
Ganeshk750 <ganesh9ot@gmail.com>
2020-09-01 14:13:54 +0530

    # adding PredicateExample 2nd Usecase

--- src/com/predicate/PredicateExample.java
+++ src/com/predicate/PredicateExample.java
@@ -7,5 +7,9 @@ public class PredicateExample {
         //If number is  > 10 return other false
         Predicate<Integer> p1 = (i) -> i > 10;
         System.out.println(p1.test(100));
+
+        //If i > 10 && number is even number (i % 2 == 0)
+        Predicate<Integer> p2 = (i) -> i % 2 == 0;
+        System.out.println(p1.and(p2).test(20));
     }
 }
