c5a5a13ab1d27da45fc2a17558305fb1a78df6a7
Ganeshk750 <ganesh9ot@gmail.com>
2020-09-02 21:27:08 +0530

    # adding BiPredicateExample

---
+++ src/com/predicate/BiPredicateExample.java
@@ -0,0 +1,24 @@
+package com.predicate;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+
+public class BiPredicateExample {
+    public static void main(String[] args) {
+        List<Instructor> instructors = Instructors.getAll();
+        BiPredicate<Boolean, Integer> p3 = (online, experience) -> online == true && experience > 10;
+
+        //BiConsumer print name and courses
+        BiConsumer<String, List<String>> biConsumer = (name, courses) ->
+            System.out.println("Name is: "+ name + " Courses: " + courses);
+
+            instructors.forEach(instructor -> {
+                if(p3.test(instructor.isOnlineCourses(), instructor.getYearOfExperience())){
+                    biConsumer.accept(instructor.getName(), instructor.getCourses());
+                }
+
+            });
+        }
+    }
+
