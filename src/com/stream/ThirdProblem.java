772b953906fdb239bb4a77b2018103a80265581a
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-06-21 14:47:13 +0530

    # Adding Problem 3 With Solution

---
+++ src/com/stream/ThirdProblem.java
@@ -0,0 +1,26 @@
+package com.stream;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * @created: 21/06/2021 - 2:39 PM
+ * @author: Ganesh
+ */
+
+
+public class ThirdProblem {
+
+    public static void main(String[] args) {
+
+        // Problem 3 : What is the average age of Male and Female Employee?
+
+        List<Employee> employeeList = TestEmployeeDb.getAllEmployees();
+        Map<String,Double> avgAgeOfMaleAndFemale = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getAge)));
+        System.out.println(avgAgeOfMaleAndFemale);
+    }
+}
