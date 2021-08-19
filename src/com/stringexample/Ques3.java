c9d053826b76e51d535d7bc104ace5033e1f8fdf
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-19 13:42:29 +0530

    # adding Ques3 solutions

---
+++ src/com/stringexample/Ques3.java
@@ -0,0 +1,26 @@
+package com.stringexample;
+
+
+/**
+ * @created: 19/08/2021 - 1:38 PM
+ * @author: Ganesh
+ */
+
+
+public class Ques3 {
+
+    public static void main(String[] args) {
+
+        String str1 = "Core";
+        String str2 = "Java";
+
+        String s1 = str1 + " " + str2;
+        System.out.println(s1);
+
+        String s2 = s1;
+        System.out.println(s2);
+
+        String s3 = s1;
+        System.out.println(s3);
+    }
+}
