c50482f0e6c0c118c11ffb4f9cfabb7553a42273
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-19 13:50:38 +0530

    ## adding Ques3 solutions

--- src/com/stringexample/Ques3.java
+++ src/com/stringexample/Ques3.java
@@ -14,13 +14,13 @@ public class Ques3 {
         String str1 = "Core";
         String str2 = "Java";
 
-        String s1 = str1 + " " + str2;
+        String s1 = str1 + str2;
         System.out.println(s1);
 
-        String s2 = s1;
+        String s2 = s1.concat(str2);
         System.out.println(s2);
 
-        String s3 = s1;
+        String s3 = new StringBuilder(str1).append(str2).toString();
         System.out.println(s3);
     }
 }
